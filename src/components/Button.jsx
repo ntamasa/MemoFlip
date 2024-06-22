1b720406934bc6253bb5f1c8e61fe1dcef443736
Tamás Nagy <ntamasalbert@gmail.com>
2024-06-22 14:25:34 +0200

    add static difficulty component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,24 @@
-function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
+import styles from "./Button.module.css";
+
+function Button({ onClick }) {
+  return (
+    <button className={styles.btn} onClick={onClick}>
+      <p className={styles.btnText}>Play</p>
+      <svg
+        className={styles.btnIcon}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m8.25 4.5 7.5 7.5-7.5 7.5"
+        />
+      </svg>
+    </button>
+  );
 }
 
 export default Button;
--- src/components/Difficulty.jsx
+++ src/components/Difficulty.jsx
@@ -1,5 +1,42 @@
+import styles from "./Difficulty.module.css";
+import Button from "./Button";
+
 function Difficulty() {
-  return <div>Difficulty</div>;
+  return (
+    <section className={styles.difficulty}>
+      <div className={styles.box}>
+        <article className={styles.col}>
+          <div className={styles.content}>
+            <h2 className={styles.heading}>
+              Normal Difficulty: Match Pairs on a 5x5 Board in 4 Minutes
+            </h2>
+            <p className={styles.text}>
+              Challenge your memory with our standard 5x5 board. You have 4
+              minutes to match all the pairs. Can you beat the clock and show
+              off your memory skills?
+            </p>
+          </div>
+
+          <Button />
+        </article>
+
+        <article className={styles.col}>
+          <div className={styles.content}>
+            <h2 className={styles.heading}>
+              Hard Difficulty: Master the 10x10 Board in 7.5 Minutes
+            </h2>
+            <p className={styles.text}>
+              Take your memory skills to the next level with our challenging
+              10x10 board. You have 7.5 minutes to find all the pairs. Do you
+              have what it takes to conquer this ultimate challenge?
+            </p>
+          </div>
+
+          <Button />
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Difficulty;
