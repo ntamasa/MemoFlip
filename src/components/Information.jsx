ebb866035ad2518c00115c676c52cfdef600761e
Tamás Nagy <ntamasalbert@gmail.com>
2024-06-25 15:42:34 +0200

    add information component styling

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,7 +1,16 @@
-import styles from "./Board.module.css";
+import styles from "./Information.module.css";
 
 function Information() {
-  return <div>Information</div>;
+  return (
+    <header className={styles.informations}>
+      <h2 className={styles.time}>Time remaining: 0:00</h2>
+
+      <div className={styles.scores}>
+        <p>Tries: 0</p>
+        <p>Score: 0</p>
+      </div>
+    </header>
+  );
 }
 
 export default Information;
