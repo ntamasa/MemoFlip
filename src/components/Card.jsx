01a77cdcae3323b4899a16b77504d2f9651dde7f
Tamás Nagy <ntamasalbert@gmail.com>
2024-06-26 12:28:22 +0200

    refactor card component, add card flip animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,44 @@
 import styles from "./Card.module.css";
 
-import imgUrl from "../../public/brain.jpg";
+import { useState } from "react";
 
 function Card() {
-  return <div className={styles.card}>&nbsp;</div>;
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isUnflipping, setIsUnflipping] = useState(false);
+
+  const handleCardClick = (e) => {
+    e.stopPropagation();
+
+    if (isFlipped) {
+      setIsUnflipping(true);
+
+      setTimeout(() => {
+        setIsFlipped(false);
+        setIsUnflipping(false);
+      }, 500);
+    } else {
+      setIsFlipped(true);
+    }
+  };
+
+  return (
+    <div className={styles.cardScene}>
+      <div
+        className={`${styles.card} ${isFlipped ? "cardUnflipped" : ""} ${
+          isUnflipping ? "cardUnflip" : ""
+        }`}
+        onClick={handleCardClick}
+      >
+        <div className={`${styles.cardFace} ${styles.cardBack}`}>
+          <div className={styles.backMain}>&nbsp;</div>
+        </div>
+
+        <div className={`${styles.cardFace} ${styles.cardFront}`}>
+          <div className={styles.mainPane}>&nbsp;</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
