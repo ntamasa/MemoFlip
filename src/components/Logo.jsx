35b3fbcbb3259380fbc87f5b72d695dcee4a3040
Tamás Nagy <ntamasalbert@gmail.com>
2024-06-27 18:37:45 +0200

    refactor Logo component for further styling

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,13 @@
 import styles from "./Logo.module.css";
 import logo from "../assets/logo.png";
 
-function Logo() {
+function Logo({ moreStyles = null }) {
   return (
-    <img className={styles.logo} src={logo} alt="Logo of the application" />
+    <img
+      className={`${styles.logo} ${moreStyles}`}
+      src={logo}
+      alt="Logo of the application"
+    />
   );
 }
 
