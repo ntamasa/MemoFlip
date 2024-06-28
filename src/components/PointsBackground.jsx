44a8e01c965e0112b67fd63a350c7be7df024a1b
Tamás Nagy <ntamasalbert@gmail.com>
2024-06-28 16:31:20 +0200

    add responsive home-points

--- src/components/PointsBackground.jsx
+++ src/components/PointsBackground.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import imgUrl from "../../public/points-background.png";
+import smallImgUrl from "../../public/points-background.png";
 
 function HomeBackground({ styles }) {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1440);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth < 1440);
+    }
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <img
       className={styles.background}
-      src={imgUrl}
+      src={isSmallScreen ? smallImgUrl : imgUrl}
       alt="Background of the section with some abstract 3D geometric shapes."
     />
   );
